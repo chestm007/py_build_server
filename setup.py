a234941ce4107a3329fffc985c6a4985e21cbee5
Max Chesterfield <chestm007@hotmail.com>
2017-03-14 03:32:25 +1100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ if sys.version > (3, ):
     sys.exit('Sorry, Python3 is not supported at this stage.')
 setup(
     name='py-build-server',
-    version='0.6.0',
+    version='0.7.0',
     packages=find_packages(),
     license='GPL3',
     author='chestm007',
